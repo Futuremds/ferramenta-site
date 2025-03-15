aee27575ca93fa6246dc8733faee6c350fc08174
Futuremds <futuremindscorp@gmail.com>
2025-03-14 22:54:17 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,1 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expõe uma API segura para o processo de renderização
-contextBridge.exposeInMainWorld('electronAPI', {
-    executarScript: (args) => ipcRenderer.invoke('editar-página', args)
-});
+let{contextBridge,ipcRenderer}=require("electron");contextBridge.exposeInMainWorld("electronAPI",{executarScript:e=>ipcRenderer.invoke("editar-página",e)});
